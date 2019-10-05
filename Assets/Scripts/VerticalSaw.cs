ff8efb3723b6f1a289de69360a9cbb3ab56c8a24
Xavier Chalut <xavier.chalut@gmail.com>
2019-10-06 01:45:39 +0200

    [FCT] Add simple vertical saw

---
+++ Assets/Scripts/VerticalSaw.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameEventSystem;
+using UnityEngine;
+
+public class VerticalSaw : MonoBehaviour
+{
+    public GameEvent onEnter;
+
+    [SerializeField] private AnimationCurve mCurve;
+    [SerializeField] private int mSize;
+    [SerializeField] private float mDuration;
+
+    private Vector3 mInitialPosition;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        mInitialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(mInitialPosition.x, mInitialPosition.y + mCurve.Evaluate(Time.time / mDuration) *mSize, mInitialPosition.z);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            onEnter.Raise();
+        }
+    }
+}
