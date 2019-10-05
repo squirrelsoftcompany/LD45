5065b5c01dbce8b0f329717b4326a0f49a994c19
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-05 12:35:42 +0200

    [QA] Rider's correction

--- Assets/Scripts/GameEventSystem/GameEventListener.cs
+++ Assets/Scripts/GameEventSystem/GameEventListener.cs
@@ -1,73 +1,76 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using System;
 
-[AddComponentMenu("GameEventListener")]
-public class GameEventListener : MonoBehaviour
+namespace GameEventSystem
 {
-    public GameEvent gameEvent;
-    public UnityEvent response;
-    public ResponseWithString responseForSentString;
-    public ResponseWithInt responseForSentInt;
-    public ResponseWithFloat responseForSentFloat;
-    public ResponseWithBool responseForSentBool;
-
-    private void OnEnable()
+    [AddComponentMenu("GameEventListener")]
+    public class GameEventListener : MonoBehaviour
     {
-        gameEvent.RegisterListener(this);
+        public GameEvent gameEvent;
+        public UnityEvent response;
+        public ResponseWithString responseForSentString;
+        public ResponseWithInt responseForSentInt;
+        public ResponseWithFloat responseForSentFloat;
+        public ResponseWithBool responseForSentBool;
+
+        private void OnEnable()
+        {
+            gameEvent.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.UnregisterListener(this);
+        }
+
+        [ContextMenu("Raise Event")]
+        public void OnEventRaised()
+        {
+            // default/generic
+            if (response.GetPersistentEventCount() >= 1)
+            {
+                // always check if at least 1 object is listening for the event
+                response.Invoke();
+            }
+
+            // string
+            if (responseForSentString.GetPersistentEventCount() >= 1)
+            {
+                responseForSentString.Invoke(gameEvent.sentString);
+            }
+
+            // int
+            if (responseForSentInt.GetPersistentEventCount() >= 1)
+            {
+                responseForSentInt.Invoke(gameEvent.sentInt);
+            }
+
+            // float
+            if (responseForSentFloat.GetPersistentEventCount() >= 1)
+            {
+                responseForSentFloat.Invoke(gameEvent.sentFloat);
+            }
+
+            // bool
+            if (responseForSentBool.GetPersistentEventCount() >= 1)
+            {
+                responseForSentBool.Invoke(gameEvent.sentBool);
+            }
+        }
     }
 
-    private void OnDisable()
-    {
-        gameEvent.UnregisterListener(this);
-    }
+    [Serializable]
+    public class ResponseWithString : UnityEvent<string> { }
 
-    [ContextMenu("Raise Event")]
-    public void OnEventRaised()
-    {
-        // default/generic
-        if (response.GetPersistentEventCount() >= 1)
-        {
-            // always check if at least 1 object is listening for the event
-            response.Invoke();
-        }
+    [Serializable]
+    public class ResponseWithInt : UnityEvent<int> { }
 
-        // string
-        if (responseForSentString.GetPersistentEventCount() >= 1)
-        {
-            responseForSentString.Invoke(gameEvent.sentString);
-        }
+    [Serializable]
+    public class ResponseWithFloat : UnityEvent<float> { }
 
-        // int
-        if (responseForSentInt.GetPersistentEventCount() >= 1)
-        {
-            responseForSentInt.Invoke(gameEvent.sentInt);
-        }
-
-        // float
-        if (responseForSentFloat.GetPersistentEventCount() >= 1)
-        {
-            responseForSentFloat.Invoke(gameEvent.sentFloat);
-        }
-
-        // bool
-        if (responseForSentBool.GetPersistentEventCount() >= 1)
-        {
-            responseForSentBool.Invoke(gameEvent.sentBool);
-        }
-    }
+    [Serializable]
+    public class ResponseWithBool : UnityEvent<bool> { }
 }
-
-[Serializable]
-public class ResponseWithString : UnityEvent<string> { }
-
-[Serializable]
-public class ResponseWithInt : UnityEvent<int> { }
-
-[Serializable]
-public class ResponseWithFloat : UnityEvent<float> { }
-
-[Serializable]
-public class ResponseWithBool : UnityEvent<bool> { }
