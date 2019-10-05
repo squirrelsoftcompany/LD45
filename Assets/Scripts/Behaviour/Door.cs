865e445631248847193dd39429e410213f0e8007
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-05 23:59:34 +0200

    [FCT] new fancy door

---
+++ Assets/Scripts/Behaviour/Door.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Behaviour
+{
+    public class Door : Button.Buttonable
+    {
+        private Animator _anim;
+        private static readonly int Open = Animator.StringToHash("Open");
+        private static readonly int Close = Animator.StringToHash("Close");
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _anim = GetComponent<Animator>();
+        }
+
+        public override void ButtonateMe()
+        {
+            _anim.SetTrigger(Open);
+        }
+
+        public override void UnbuttonateMe()
+        {
+            _anim.SetTrigger(Close);
+        }
+    }
+}
