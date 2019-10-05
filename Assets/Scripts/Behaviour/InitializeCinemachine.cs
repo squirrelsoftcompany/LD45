7a0be238b26179cfdbfcf4c1a86a467ffe17c4c7
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-06 01:33:24 +0200

    [DATA] prefab Cinemachine

---
+++ Assets/Scripts/Behaviour/InitializeCinemachine.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Behaviour
+{
+    [RequireComponent(typeof(Cinemachine.CinemachineVirtualCamera))]
+    public class InitializeCinemachine : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var machine = GetComponent<Cinemachine.CinemachineVirtualCamera>();
+            var mask = GameObject.FindWithTag("Mask");
+            machine.Follow = mask.transform;
+            machine.LookAt = mask.transform;
+
+            enabled = false;
+        }
+    }
+}
