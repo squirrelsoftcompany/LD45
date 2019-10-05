e488e6406d5bbb5140005a7b80b20fac017f9cdb
OroshiX <oroshix@users.noreply.github.com>
2019-10-05 20:36:19 +0200

    [FCT] On suicide, activate flammable and deactivate attack and movement

--- Assets/Scripts/Control/Suicide.cs
+++ Assets/Scripts/Control/Suicide.cs
@@ -7,11 +7,9 @@ using UnityEngine.Serialization;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
-namespace Control
-{
+namespace Control {
     [RequireComponent(typeof(Collider2D))]
-    public class Suicide : MonoBehaviour
-    {
+    public class Suicide : MonoBehaviour {
         // new pig creation information
         public GameObject pigPrefab;
         private GameObject _spawn;
@@ -25,10 +23,9 @@ namespace Control
         private Movement _movement;
 
         // Start is called before the first frame update
-        private void Start()
-        {
+        private void Start() {
             _spawn = GameObject.FindWithTag("Respawn");
-            
+
             _mask = gameObject;
             _maskCollider2D = _mask.GetComponent<Collider2D>();
 
@@ -38,51 +35,55 @@ namespace Control
         }
 
         // Update is called once per frame
-        private void Update()
-        {
+        private void Update() {
             bool suicideRequested = Input.GetButtonUp("Suicide");
             if (!suicideRequested && !_dying) return;
 
-            if (suicideRequested && !_dying)
-            {
+            if (suicideRequested && !_dying) {
                 // deactivate happyArea triggering
                 _maskCollider2D.enabled = false;
                 
-                // deactivate previous controller
-                _movement.enabled = false;
-                
-                // todo "kill" previous pig
+                // "kill" previous pig
+                killPig(_currentPig);
 
                 _currentPig = Instantiate(pigPrefab, _spawn.transform.position, Quaternion.identity, _spawn.transform);
                 _movement = _currentPig.GetComponent<Movement>();
                 _movement.enabled = false;
 
                 _maskParent = null;
-                foreach (Transform child in _currentPig.transform)
-                {
+                foreach (Transform child in _currentPig.transform) {
                     if (!child.gameObject.CompareTag("MaskParent")) continue;
 
                     _maskParent = child.gameObject;
                     break;
                 }
+
                 Debug.Assert(_maskParent, "no mask parent found");
 
                 _dying = true;
             }
 
             if (!_dying) return;
-            
+
             var position = _mask.transform.position;
             position = Vector3.Slerp(position, _maskParent.transform.position, Time.deltaTime * 2);
             _mask.transform.position = position;
 
             if (!(Vector3.Distance(position, _maskParent.transform.position) <= 0.01)) return;
-            
+
             _mask.transform.SetParent(_maskParent.transform);
-                    
+
             _dying = false;
             _maskCollider2D.enabled = true;
             _movement.enabled = true;
+        }
+
+        private void killPig(GameObject pig) {
+            // TODO "kill" previous pig
+            pig.GetComponent<Flammable>().enabled = true;
+            pig.GetComponent<Attack>().enabled = false;
+            // deactivate previous controller
+            pig.GetComponent<Movement>().enabled = false;
         }
     }
 }
