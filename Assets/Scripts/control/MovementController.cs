1232737e1c6ed80149d593fb72fd479dc3acd99e
OroshiX <oroshix@users.noreply.github.com>
2019-10-05 16:10:01 +0200

    [WIP] Remove Input master

--- Assets/Scripts/control/MovementController.cs
+++ Assets/Scripts/control/MovementController.cs
@@ -1,34 +1,18 @@
-﻿using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using System;
+using UnityEngine;
 
 namespace control {
     [RequireComponent(typeof(Rigidbody2D))]
-    public class MovementController : MonoBehaviour, InputMaster.IPlayerActions {
+    public class MovementController : MonoBehaviour {
         [SerializeField] private float speed;
-        private InputMaster controls;
 
         private Vector3 direction;
         private Rigidbody2D rb2D;
+        private const double TOLERANCE = 0.0003;
 
         // Start is called before the first frame update
         private void Start() {
             rb2D = GetComponent<Rigidbody2D>();
-        }
-
-        private void Awake() {
-            controls = new InputMaster();
-            controls.Player.SetCallbacks(this);
-//            controls.Player.Jump.performed += _ => jump();
-//            controls.Player.Movement.
-        }
-
-//
-        private void OnEnable() {
-            controls.Player.Enable();
-        }
-
-        private void OnDisable() {
-            controls.Player.Disable();
         }
 
         public void jump() {
@@ -40,43 +24,38 @@ namespace control {
             Debug.Log("suicide");
         }
 
-        public void move(InputAction.CallbackContext context) {
-            // todo
-            Debug.Log("Move: " + context);
-        }
-
         private void FixedUpdate() {
-//            var translationSecond = direction * speed;
-//            rb2D.MovePosition(transform.position + Time.fixedDeltaTime * translationSecond);
-//            if (direction.x < 0f) {
-//                var transform1 = transform;
-//                var transformLocalRotation = transform1.localRotation;
-//                transformLocalRotation.x = -transformLocalRotation.x;
-//                transform1.localRotation = transformLocalRotation;
-//            }
+            var translationSecond = direction * speed;
+            rb2D.MovePosition(transform.position + Time.fixedDeltaTime * translationSecond);
+            if (direction.x < 0f) {
+                var transform1 = transform;
+                var transformLocalRotation = transform1.localRotation;
+                transformLocalRotation.x = -transformLocalRotation.x;
+                transform1.localRotation = transformLocalRotation;
+            }
         }
 
         // Update is called once per frame
         private void Update() {
-//            var moveHorizontal = Input.GetAxis("Horizontal");
-//            var moveVertical = Input.GetAxis("Jump");
-//
-//            direction = new Vector3(moveHorizontal, moveVertical, 0f).normalized;
+            var moveHorizontal = Input.GetAxis("Horizontal");
+            var moveVertical = Input.GetAxis("Jump");
+
+            if (Math.Abs(moveHorizontal) > TOLERANCE || Math.Abs(moveVertical) > TOLERANCE) {
+                direction = new Vector3(moveHorizontal, moveVertical, 0f).normalized;
+            }
         }
 
-        public void OnJump(InputAction.CallbackContext context) {
+        public void OnFire() {
             jump();
         }
 
-        public void OnMovement(InputAction.CallbackContext context) {
-            move(context);
+        void onSuicide() { }
+
+        public void OnMove() {
+            Debug.Log("MOVE! ");
         }
 
-        public void OnSuicide(InputAction.CallbackContext context) {
-            suicide();
-        }
-
-        public void OnCombustion(InputAction.CallbackContext context) {
+        public void OnCombustion() {
             Debug.Log("combustion");
         }
     }
