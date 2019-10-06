7aa103c312ba0aafbc184283025c1db326373f4d
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-06 19:55:35 +0200

    [FCT] some UI elements

---
+++ Assets/Scripts/UI/GeneralMixerManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace UI {
+    public class GeneralMixerManager : MonoBehaviour
+    {
+        public AudioMixer mixer;
+
+        public Slider sliderMaster;
+        public Slider sliderMusic;
+        public Slider sliderSfx;
+
+        private void Start()
+        {
+            mixer.GetFloat("MasterVolume", out var value);
+            sliderMaster.value = value;
+            mixer.GetFloat("MusicVolume", out var value2);
+            sliderMusic.value = value2;
+            mixer.GetFloat("SoundEffectsVolume", out var value3);
+            sliderSfx.value = value3;
+        }
+        
+        public void ChangeGeneral()
+        {
+            mixer.SetFloat("MasterVolume", sliderMaster.value);
+        }
+        
+        public void ChangeMusic()
+        {
+            mixer.SetFloat("MusicVolume", sliderMusic.value);
+        }
+        
+        public void ChangeSfx()
+        {
+            mixer.SetFloat("SoundEffectsVolume", sliderSfx.value);
+        }
+    }
+}
