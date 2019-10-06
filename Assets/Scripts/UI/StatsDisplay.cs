8b1b64ef1cc120cd436f63e60d204f59336d667e
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-06 20:41:00 +0200

    [FCT] display of stats in UI

---
+++ Assets/Scripts/UI/StatsDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+namespace UI
+{
+    public class StatsDisplay : MonoBehaviour
+    {
+        public TextMeshProUGUI bodyCountText;
+        public TextMeshProUGUI timeText;
+
+        public Stats.Stats toDisplay;
+        
+        private void FixedUpdate()
+        {
+            bodyCountText.text = $"{toDisplay.bodyCount:0000}";
+            
+            var ts = TimeSpan.FromSeconds(toDisplay.time);
+            timeText.text = $"{ts.Minutes:D2}:{ts.Seconds:D2}";
+        }
+    }
+}
