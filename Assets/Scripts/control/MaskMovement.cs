90ba215ab39919bf36331ff575f1b5ae4d9fcab8
OroshiX <oroshix@users.noreply.github.com>
2019-10-06 20:37:00 +0200

    Mask movement (independent)

--- Assets/Scripts/control/MaskMovement.cs
+++ Assets/Scripts/control/MaskMovement.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
 
 namespace control {
+    [RequireComponent(typeof(Collider2D))]
     public class MaskMovement : MonoBehaviour {
         [SerializeField] private float speed;
+        private Rigidbody2D rb2D;
+
+        private void Start() {
+            rb2D = GetComponent<Rigidbody2D>();
+        }
 
         private void FixedUpdate() {
             var vectorDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
 
-            var transform1 = transform;
-            var position = transform1.position;
-            var theSpeed = Time.deltaTime * speed;
-            position.x += vectorDir.x * theSpeed;
-            position.y += vectorDir.y * theSpeed;
-
-            transform1.position = position;
+            var theSpeed = Time.fixedDeltaTime * speed;
+            rb2D.velocity = vectorDir * theSpeed;
         }
     }
 }
