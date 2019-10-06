042273353de55b50c3cc162e0ea0a211d2b660eb
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-06 20:39:57 +0200

    [FCT] some stats objects

---
+++ Assets/Scripts/Stats/Stats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Stats
+{
+    [CreateAssetMenu(fileName = "Stats", menuName = "Stats")]
+    public class Stats : ScriptableObject
+    {
+        public int bodyCount = 0;
+        public float time = 0;
+    }
+}
