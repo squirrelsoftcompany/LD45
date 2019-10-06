5113a71f83e89f602c2a8686f996a987c32c8dcb
Xavier Chalut <xavier.chalut@gmail.com>
2019-10-06 14:20:38 +0200

    [FCT] Butcher is now really dangerous ! ( Saw too )

--- Assets/Scripts/Butcher/Butcher.cs
+++ Assets/Scripts/Butcher/Butcher.cs
@@ -13,20 +13,25 @@ public class Butcher : MonoBehaviour
 
     private Vector3 mDirection = Vector3.zero;
     private Rigidbody2D mRb2D;
+    private bool mSlowed = false;
+    private GameObject mCleaver;
 
     // Start is called before the first frame update
     void Start()
     {
         mRb2D = GetComponent<Rigidbody2D>();
+        mCleaver = transform.Find("Cleaver").gameObject;
     }
 
     private void FixedUpdate()
     {
         Vector3 lTranslationSecond = mDirection * mAcceleration;
         mRb2D.AddForce(Time.fixedDeltaTime * lTranslationSecond);
-        if (Mathf.Abs(mRb2D.velocity.x) > mMaxSpeed)
-            mRb2D.velocity = new Vector2(mRb2D.velocity.normalized.x * mMaxSpeed, mRb2D.velocity.y);
-
+        float lCurrentMaxSpeed = (mSlowed ? mMaxSpeed * 0.5f : mMaxSpeed);
+        if (Mathf.Abs(mRb2D.velocity.x) > lCurrentMaxSpeed)
+        {
+            mRb2D.velocity = new Vector2(mRb2D.velocity.x * lCurrentMaxSpeed, mRb2D.velocity.y);
+        }
     }
 
     // Update is called once per frame
@@ -41,7 +46,7 @@ public class Butcher : MonoBehaviour
         int layerMask = 1 << 1; // TODO : Use the correct layer mask !! Currently, it is just hitting itself....
         layerMask = ~layerMask;
 
-        Vector3 lViewDirection = Vector3.Normalize(lTargetTransform.position- transform.position);
+        Vector3 lViewDirection = Vector3.Normalize(lTargetTransform.position - transform.position);
         // Does the ray intersect any objects excluding the butcher layer
         RaycastHit2D lHit = Physics2D.Raycast(transform.position, transform.TransformDirection(lViewDirection), Mathf.Infinity, layerMask);
         if (lHit.collider != null)
@@ -55,10 +60,32 @@ public class Butcher : MonoBehaviour
         if (lVisible && lDistanceFromTarget < mMaxTargetDistance && lDistanceFromTarget > lMinDistance)
         {
             mDirection = lTargetTransform.position.x > transform.position.x ? Vector3.right : Vector3.left;
-        } 
+        }
         else
         {
             mDirection = Vector3.zero;
+        }
+
+        //If very close, try to hit the pig
+        if (lDistanceFromTarget <= lMinDistance + Mathf.Epsilon)
+        {
+            //Trigger attack animation -> active the cleaver child object
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 12) // 12 is body layer
+        {
+            mSlowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 12)// 12 is body layer
+        {
+            mSlowed = false;
         }
     }
 }
---
+++ Assets/Scripts/Butcher/Cleaver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameEventSystem;
+using UnityEngine;
+
+public class Cleaver : MonoBehaviour
+{
+    public GameEvent onEnter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            onEnter.Raise();
+        }
+    }
+}
