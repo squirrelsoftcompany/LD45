84ee30dbcb23bdf78d8e1a6e965f536be36d31a8
Xavier Chalut <xavier.chalut@gmail.com>
2019-10-06 15:08:10 +0200

    [FCT] Stop the saws !

--- Assets/Scripts/VerticalSaw.cs
+++ Assets/Scripts/VerticalSaw.cs
@@ -3,33 +3,54 @@ using System.Collections.Generic;
 using GameEventSystem;
 using UnityEngine;
 
-public class VerticalSaw : MonoBehaviour
+
+namespace Behaviour
 {
-    public GameEvent onEnter;
-
-    [SerializeField] private AnimationCurve mCurve;
-    [SerializeField] private int mSize;
-    [SerializeField] private float mDuration;
-
-    private Vector3 mInitialPosition;
-    // Start is called before the first frame update
-
-    void Start()
+    public class VerticalSaw : Button.Buttonable
     {
-        mInitialPosition = transform.position;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = new Vector3(mInitialPosition.x, mInitialPosition.y + mCurve.Evaluate(Time.time / mDuration) *mSize, mInitialPosition.z);
-    }
-    
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.gameObject.CompareTag("Player"))
+        public GameEvent onEnter;
+
+        [SerializeField]
+        private AnimationCurve mCurve;
+        [SerializeField]
+        private int mSize;
+        [SerializeField]
+        private float mDuration;
+        [SerializeField]
+        private bool mActivated = false;
+
+        private Vector3 mInitialPosition;
+        // Start is called before the first frame update
+
+        void Start()
         {
-            onEnter.Raise();
+            mInitialPosition = transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (mActivated)
+                transform.position = new Vector3(mInitialPosition.x, mInitialPosition.y + mCurve.Evaluate(Time.time / mDuration) * mSize, mInitialPosition.z);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                onEnter.Raise();
+            }
+        }
+
+        public override void ButtonateMe()
+        {
+            mActivated = false;
+        }
+
+        public override void UnbuttonateMe()
+        {
+            mActivated = true;
         }
     }
 }
