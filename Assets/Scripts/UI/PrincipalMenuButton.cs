6ea7cf03a4d8bc0eb96c0bd11fbf8860011d49c6
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-07 17:10:49 +0200

    [FCT] Start Game button is now working

---
+++ Assets/Scripts/UI/PrincipalMenuButton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class PrincipalMenuButton : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            // goto level1
+            SceneManager.LoadScene(1);
+        }
+    }
+}
