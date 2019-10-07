e95d1d82962f9a07aa11020684c44d66d4076ff5
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-07 15:48:50 +0200

    [QA] move to namespace

--- Assets/Scripts/UI/MenuButton.cs
+++ Assets/Scripts/UI/MenuButton.cs
@@ -1,72 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuButton : MonoBehaviour
+namespace UI
 {
-    public GameObject menu;
-    public GameObject header;
-    public GameObject finalScreen;
-
-    private bool _paused = false;
-    private bool _levelFinished = false;
-    
-    // Update is called once per frame
-    void Update()
+    public class MenuButton : MonoBehaviour
     {
-        if (_levelFinished) return;
-        
-        if (Input.GetButtonUp("Pause"))
+        public GameObject menu;
+        public GameObject header;
+        public GameObject finalScreen;
+
+        private bool _paused = false;
+        private bool _levelFinished = false;
+    
+        // Update is called once per frame
+        void Update()
         {
-            if (!_paused)
-                Pause();
-            else
-                Resume();
+            if (_levelFinished) return;
+        
+            if (Input.GetButtonUp("Pause"))
+            {
+                if (!_paused)
+                    Pause();
+                else
+                    Resume();
+            }
         }
-    }
 
-    //*** Menu's buttons
+        //*** Menu's buttons
     
-    public void Pause()
-    {
-        _paused = true;
-        Time.timeScale = 0;
-        menu.SetActive(true);
-    }
+        public void Pause()
+        {
+            _paused = true;
+            Time.timeScale = 0;
+            menu.SetActive(true);
+        }
 
-    public void Resume()
-    {
-        _paused = false;
-        Time.timeScale = 1;
-        menu.SetActive(false);
-    }
+        public void Resume()
+        {
+            _paused = false;
+            Time.timeScale = 1;
+            menu.SetActive(false);
+        }
 
-    public void RestartLevel()
-    {
-        Time.timeScale = 1;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+        public void RestartLevel()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     
-    public void RestartGame()
-    {
-        // Time.timeScale = 1;
-        // SceneManager.LoadScene(FirstLevelName);
-    }
+        public void RestartGame()
+        {
+            // Time.timeScale = 1;
+            // SceneManager.LoadScene(FirstLevelName);
+        }
     
-    //***  final screen's button
+        //***  final screen's button
 
-    public void LevelFinished()
-    {
-        _levelFinished = true;
-        Time.timeScale = 0;
-        finalScreen.SetActive(true);
-        header.SetActive(false);
-        menu.SetActive(false);
-    }
-    public void NextLevel()
-    {
-        // Time.timeScale = 1;
-        // SceneManager.LoadScene(nextLevelName);
+        public void LevelFinished()
+        {
+            _levelFinished = true;
+            Time.timeScale = 0;
+            finalScreen.SetActive(true);
+            header.SetActive(false);
+            menu.SetActive(false);
+        }
+        public void NextLevel()
+        {
+            // Time.timeScale = 1;
+            // SceneManager.LoadScene(nextLevelName);
+        }
     }
 }
