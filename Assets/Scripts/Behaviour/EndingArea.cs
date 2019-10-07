cfe8cad1783b2679859bd5e8c34bf76289743b8b
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-07 03:53:27 +0200

    [FCT] Ending Area

---
+++ Assets/Scripts/Behaviour/EndingArea.cs
@@ -0,0 +1,15 @@
+﻿using GameEventSystem;
+using UnityEngine;
+
+namespace Behaviour
+{
+    public class EndingArea : MonoBehaviour
+    {
+        public GameEvent onEnter;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            onEnter.Raise();
+        }
+    }
+}
