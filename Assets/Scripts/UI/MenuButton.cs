0aaedf95edc41890d2739cd945967970766ef83e
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-07 03:06:31 +0200

    [FCT] menu is now part of new MainCam prefab and there is a new prefab called FinalScreen

--- Assets/Scripts/UI/MenuButton.cs
+++ Assets/Scripts/UI/MenuButton.cs
@@ -1,46 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour
 {
     public GameObject menu;
+    public GameObject header;
+    public GameObject finalScreen;
 
+    private bool _paused = false;
+    private bool _levelFinished = false;
+    
     // Update is called once per frame
     void Update()
     {
+        if (_levelFinished) return;
+        
         if (Input.GetButtonUp("Pause"))
         {
-            if (Time.timeScale > 0)
-            {
+            if (!_paused)
                 Pause();
-            }
             else
-            {
                 Resume();
-            }
         }
     }
 
+    //*** Menu's buttons
+    
     public void Pause()
     {
+        _paused = true;
         Time.timeScale = 0;
         menu.SetActive(true);
     }
-    
+
     public void Resume()
     {
+        _paused = false;
         Time.timeScale = 1;
         menu.SetActive(false);
     }
-    
+
     public void RestartLevel()
     {
-        // restart level
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     
     public void RestartGame()
     {
-        // restart level
+        // Time.timeScale = 1;
+        // SceneManager.LoadScene(FirstLevelName);
+    }
+    
+    //***  final screen's button
+
+    public void LevelFinished()
+    {
+        _levelFinished = true;
+        Time.timeScale = 0;
+        finalScreen.SetActive(true);
+        header.SetActive(false);
+        menu.SetActive(false);
+    }
+    public void NextLevel()
+    {
+        // Time.timeScale = 1;
+        // SceneManager.LoadScene(nextLevelName);
     }
 }
