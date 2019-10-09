10c878ad6907729510c59eff22992b57b3ae5c7e
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-09 23:48:50 +0200

    [FCT] "press X to let the pig where it is"

---
+++ Assets/Scripts/UI/TutorialDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialDisplay : MonoBehaviour
+{
+    public GameObject text;
+    public Stats.Stats stats;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (stats.bodyCount > 0) return;
+        text.SetActive(true);
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        text.SetActive(false);
+    }
+}
