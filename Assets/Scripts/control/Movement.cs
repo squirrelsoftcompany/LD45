8083059ede0ce4a0f800dee62119cb054557496f
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-10-10 02:13:32 +0200

    [BUG] more accurate animation for the piggy

--- Assets/Scripts/control/Movement.cs
+++ Assets/Scripts/control/Movement.cs
@@ -12,19 +12,16 @@ namespace control {
         public LayerMask rayLayerMask;
         public LayerMask rayLayerMaskDead;
         private Rigidbody2D rb;
-        private bool facingRight = true;
         private Animator animator;
-        private static readonly int JUMP = Animator.StringToHash("jump");
+        private static readonly int JUMP = Animator.StringToHash("jumping");
         private static readonly int RUN = Animator.StringToHash("run");
         private bool toBeDisabled;
+
+        private bool _jumping = false;
 
         private void Start() {
             rb = GetComponent<Rigidbody2D>();
             animator = GetComponent<Animator>();
-
-            // initial flip if needed
-            if (facingRight && Math.Sign(transform.localScale.x) < 0)
-                flip();
         }
 
         private bool isGrounded() {
@@ -72,41 +69,36 @@ namespace control {
                 enabled = false;
                 return;
             }
-
-            var x = Input.GetAxis("Horizontal");
+            
             if (Input.GetAxis("Jump") > 0) {
                 if (isGrounded()) {
                     var rbVelocity = rb.velocity;
                     rbVelocity.y = jump;
                     rb.velocity = rbVelocity;
-//                    rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-                    animator.SetTrigger(JUMP);
+                    _jumping = true;
+                    animator.SetBool(JUMP, true);
                 }
             }
+            else if (_jumping && isGrounded())
+            {
+                _jumping = false;
+                animator.SetBool(JUMP, false);
+            }
 
+            var h = Input.GetAxis("Horizontal");
             var velocity = rb.velocity;
-            velocity = new Vector3(x * speed, velocity.y, 0);
+            velocity = new Vector3( h * speed, velocity.y, 0);
             rb.velocity = velocity;
-            if (Mathf.Abs(velocity.y) > 0f) {
-                animator.SetTrigger(JUMP);
-            } else {
-                animator.SetBool(RUN, velocity.sqrMagnitude > 0);
+            
+            animator.SetBool(RUN, Mathf.Abs(h) > 0f /*&& velocity.sqrMagnitude > 0.3f*/);
+
+            // flip pig if necessary
+            if (Mathf.Abs(rb.velocity.x) > 0.1f)
+            {
+                var scale = transform.localScale;
+                scale.x = Math.Abs(scale.x) * Mathf.Sign(rb.velocity.x);
+                transform.localScale = scale;
             }
-
-            if (rb.velocity.x > 0 && !facingRight) {
-                flip();
-            } else if (rb.velocity.x < 0 && facingRight) {
-                flip();
-            }
-        }
-
-        private void flip() {
-            facingRight = !facingRight;
-
-            var transform1 = transform;
-            var theScale = transform1.localScale;
-            theScale.x = Math.Abs(theScale.x) * (facingRight ? 1 : -1);
-            transform1.localScale = theScale;
         }
     }
 }
